f755e4c6c5c8a1afdd1b978008e48c2931a782ea
Fatimaa <36267070+Fatimuskii@users.noreply.github.com>
2020-10-13 00:50:48 +0200

    Ejercicio 2 DONE

---
+++ Prob1 - Número de máximos/problema1.cpp
@@ -0,0 +1,73 @@
+﻿// Nombre del alumno Fátima García
+// Usuario del Juez E24
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+/*
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}
+
+*/
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+
+    int num;
+    int max; 
+    int cont = 0;
+
+    std::cin >> num; 
+    max = num; // asignamos el max al primer elemento 
+
+    while (num != 0) {
+        
+        if (num > max) {// comparamos el max por si tenemos que actualizar
+            max = num; 
+            cont = 1; 
+        }
+        else if (num == max) { // si conicide con el max incrementamos su contador
+            cont++;
+        }
+        //leemos el siguiente numero
+        std::cin >> num;
+
+    }
+ 
+    // escribir sol
+    std::cout << max << " " << cont << '\n'; 
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Prob2 - Esta bien dividido el vector/problema2.cpp
@@ -0,0 +1,81 @@
+﻿// Nombre del alumno Fátima G. 
+// Usuario del Juez E24
+
+
+// ¿ Está bien dividido el vector ?
+
+// O(n): lineal respecto al numero de elementos del vector
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+
+// función que resuelve el problema
+bool resolver(std::vector<int> const& v, int pos) {
+
+    int max = v[0]; // max desde [0-p]
+
+    // recorremos de [0-p] y nos quedamos con el valor maximo.
+    for (int i = 0; i <= pos; ++i) {
+        if (v[i] > max) {
+            max = v[i]; 
+        }
+    }
+
+    // recorremos de [p+1 - n-1] comprobando que sea < que el max 
+    int j = pos + 1;
+    while (j < v.size()) {
+        if (v[j] <= max)
+            return false;
+        ++j;
+    }
+
+    return true;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+
+    int numElementos, pos, num; 
+    std::cin >> numElementos >> pos; 
+
+    std::vector<int> vector; 
+    for (int i = 0; i < numElementos; ++i){
+        std::cin >> num;
+        vector.push_back(num); 
+    }
+
+    bool sol = resolver(vector, pos);
+    // escribir sol
+    std::cout << (sol ? "SI\n" : "NO\n"); 
+
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
