85919473cbe3f3aa1806e966bc3852b16fba8922
Fatimuskii <fatimaig@ucm.es>
2020-12-29 02:43:25 +0100

    Ejercicios VA

---
+++ Vuelta atrás/47C Torres de colores/problema47C.cpp
@@ -0,0 +1,140 @@
+﻿// Nombre del alumno Fatima García Delgado   
+// Usuario del Juez E24
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+// Torres de colores.
+
+// Colores disponibles: azul(0), rojo(1) y verde(2)
+int const COLORES = 3;
+
+struct tSol {
+    bool haySol; 
+    std::vector<int> v; 
+};
+//Condicion que comprueba que no se colocan dos piezas verdes seguidas. 
+bool esValida(std::vector<int>piezasDisponibles, std::vector<int> contadorPiezasUsadas, tSol sol, int colorActual, int pos) {
+    
+    //No quedan piezas disponibles de la pieza que toca
+    if (piezasDisponibles[colorActual] < 1)
+        return false; 
+    //Dos piezas verdes juntas
+    if (colorActual == 2 && colorActual == sol.v[pos - 1])
+        return false;
+    // Numero de piezas verdes > piezas azules
+    if (colorActual == 2 && contadorPiezasUsadas[2]+1 > contadorPiezasUsadas[0])
+        return false;
+    
+    return true;
+}
+
+
+//Mostrar por pantalla la soluciobn
+void escribirSolucion(std::vector<int> contadorPiezasUsadas, tSol &sol) {
+
+    // Comprobar si numero de piezas rojas > suma (azules y verdes)
+    if (contadorPiezasUsadas[1] > (contadorPiezasUsadas[0] + contadorPiezasUsadas[2])) {
+        sol.haySol = true;
+        for (int c : sol.v) {
+            switch (c) {
+            case 0:
+                std::cout << "azul" << ' ';
+                break;
+            case 1:
+                std::cout << "rojo" << ' ';
+                break;
+            case 2:
+                std::cout << "verde" << ' ';
+                break;
+            }
+        }
+        std::cout << '\n';
+    }
+  
+}
+
+// función que resuelve el problema
+void resolver(int altura, std::vector<int> piezasDisponibles, int k, std::vector<int> &contadorPiezasUsadas, tSol&sol) {
+
+    for (int i = 0; i < COLORES; ++i) {
+        //sol
+        if (esValida(piezasDisponibles, contadorPiezasUsadas, sol, i, k)) {
+            sol.v[k] = i;
+            piezasDisponibles[i]--; 
+            contadorPiezasUsadas[i]++;
+            if (k == altura - 1) { // Es solucion (n: tam de la solucion)
+                escribirSolucion(contadorPiezasUsadas, sol);
+            }
+            else { // seguimos construyendo la solucion 
+                resolver(altura,piezasDisponibles, k + 1, contadorPiezasUsadas, sol);
+            }
+            piezasDisponibles[i]++;
+            contadorPiezasUsadas[i]--;
+        }
+    }
+    
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int altura;
+    std::cin >> altura;
+    if (altura == 0)
+        return false;
+
+    std::vector<int>piezasDisponibles(COLORES, -1);
+    for (int i = 0; i < COLORES; ++i) {
+        std::cin >> piezasDisponibles[i];
+    }
+
+    // Generar las soluciones
+    tSol sol;
+    std::vector<int>vector(altura, -1);
+    sol.v = vector;
+    
+    std::vector<int> contadorPiezasUsadas(COLORES, 0); 
+
+    // Primera pieza es siempre roja
+
+    sol.v[0] = 1;
+    sol.haySol = false;
+
+    piezasDisponibles[1]--; 
+    contadorPiezasUsadas[1]++;
+    resolver(altura, piezasDisponibles, 1, contadorPiezasUsadas, sol);
+ 
+    if (!sol.haySol)
+        std::cout << "SIN SOLUCION\n";
+
+    std::cout << '\n';
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("47Csample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Vuelta atrás/48 Necesito un abrigo/problema48.cpp
@@ -0,0 +1,98 @@
+﻿// Nombre del alumno Fatima Garcia Delgado	
+// Usuario del Juez E24
+
+//Necesito un abrigo
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+bool esValida(std::vector<int> const& precipitaciones, std::vector<int> const& cant,
+	std::vector<int> const& sol, std::vector<int>& marcar, int k, int i) {
+
+	if ((k >= 1) && (sol[k - 1] == sol[k])) return false; //dos seguidas;
+	if (precipitaciones[k] > cant[sol[k]]) return false; //se moja;
+	if (marcar[i] > (k / 3) + 2) return false; //abrigo mas usado < (un tercio + 2 dias transcuridos):
+
+	return true;
+}
+
+
+// función que resuelve el problema
+void resolver(int numDias, int numAbrigos, std::vector<int> const& precip,
+	std::vector<int> const& cant, std::vector<int>& sol, int& cont,
+	std::vector<int>& marcar, int k) {
+
+	for (int i = 0; i < numAbrigos; ++i) {
+		sol[k] = i; //Guardo un abrigo en un dia en concreto;
+		++marcar[i]; //Marco el abrigo usado;
+
+		if (esValida(precip, cant, sol, marcar, k, i)) {
+			if (k == numDias - 1) { //final?
+				if (sol[0] != sol[k]) { //abrigo distinto el primer y ultimo dia;
+					cont++;
+				}
+			}
+
+			else resolver(numDias, numAbrigos, precip, cant, sol, cont, marcar, k + 1); //siguiente etapa;
+		}
+
+		sol[k] = -1; //Quito el abrigo cuando no es valido
+		--marcar[i]; //Desmarco el abrigo usado;
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int numDias, numAbrigos;
+
+	std::cin >> numDias >> numAbrigos;
+	if (numDias == 0 && numAbrigos == 0)
+		return false;
+
+
+	std::vector<int> precip(numDias);
+	for (int i = 0; i < numDias; ++i)
+		std::cin >> precip[i];
+
+	std::vector<int> cant(numAbrigos);
+	for (int i = 0; i < numAbrigos; ++i)
+		std::cin >> cant[i];
+
+
+	std::vector<int> sol(numDias, -1);
+	std::vector<int> marcaje(numAbrigos);
+	int cont = 0;
+	resolver(numDias, numAbrigos, precip, cant, sol, cont, marcaje, 0);
+
+	// escribir sol
+	if (cont == 0) std::cout << "Lo puedes comprar\n";
+	else std::cout << cont << '\n';
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("48sample.in");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
