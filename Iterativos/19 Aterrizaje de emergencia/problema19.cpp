413e07d3708f4759ee69cac689a535d224e73952
Fatimaa <36267070+Fatimuskii@users.noreply.github.com>
2021-02-04 04:20:51 +0100

    Problema 21 WA

---
+++ Iterativos/19 Aterrizaje de emergencia/problema19.cpp
@@ -0,0 +1,89 @@
+﻿// Nombre del alumno Fatima Garcia 
+// Usuario del Juez E24
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include<vector> 
+
+
+// Atterrizaje de emergencia
+
+// función que resuelve el problema
+void resolver(std::vector<int> const& v, int const& minValores, std::vector<int>&sol ) {
+
+    int cont = 1; 
+   
+    for (int i = 1; i < v.size(); ++i) {
+
+        if (std::abs(v[i] - v[i - 1]) == 1 || std::abs(v[i] - v[i - 1]) == 0) {
+
+            cont++;
+            if (cont >= minValores) {
+                if (sol.empty() || sol[sol.size()-1]!= (i-cont+1)) {
+                    sol.push_back(i - cont + 1);
+                }   
+            }
+        }
+        else {
+            cont = 1;
+        }
+           
+
+
+    }
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+
+    int numDatos, minValores; // num de datos tomados || cantidad de calores casi iguales que se necesitan
+    std::cin >> numDatos >> minValores; 
+    if (numDatos == 0)
+        return false;
+
+    std::vector<int> valoresTomados(numDatos); 
+    for (int i = 0; i < numDatos; ++i) {
+        std::cin >> valoresTomados[i];
+    }
+
+    std::vector<int>sol; // vector donde guardo los indices de las soluciones
+    resolver(valoresTomados, minValores, sol);
+
+    // escribir sol
+    if (sol.empty()) std::cout << "0"; 
+    else {
+        std::cout << sol.size() <<" " << sol[0];
+        for (int i = 1; i < sol.size(); ++i)
+            std::cout << " " << sol[i]; 
+    }
+
+    std::cout << '\n'; 
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("19sample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Iterativos/21 Mejorar tu mejor marca/problema21.cpp
@@ -0,0 +1,96 @@
+﻿// Nombre del alumno Fatima Garcia Delgado
+// Usuario del Juez E24
+
+
+// Cuanto tiempo llevas sin batir tu mejor marca?
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+struct tSol {
+    int maxPruebasMalas; // maximo de pruebas en q el atleta no ha mejorado su mejor marca HASTa el momento
+    int comienzo; // comienzo del ultimo periodo maximo de pruebas 
+};
+
+// función que resuelve el problema
+tSol resolver(std::vector<int> const&v) {
+    int mejorMarca = v[0];
+    int ini = 0;
+    int contPruebasMalas = 0;
+    int maxMalas = -1; 
+
+    for (int i = 1; i < v.size(); i++) {
+
+        if (v[i] <= mejorMarca) {
+            contPruebasMalas++;
+            if(ini != i - contPruebasMalas +1)
+                ini = i-contPruebasMalas+1;
+        }
+
+        else {
+            if (v[i] > mejorMarca) {
+                mejorMarca = v[i];
+                if (contPruebasMalas > maxMalas) {
+                    maxMalas = contPruebasMalas;
+                    ini = i - maxMalas + 1;
+                }
+                contPruebasMalas = 0;
+            }
+        }
+    }
+    if (maxMalas == -1)
+        maxMalas = contPruebasMalas;
+    else if (maxMalas == 0) 
+        ini = v.size();
+    
+    return { maxMalas, ini };
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int numMarcas; 
+    std::cin >> numMarcas; 
+
+    if (numMarcas == 0)
+        return false;
+    std::vector<int> marcas(numMarcas); 
+
+    for (int i = 0; i < numMarcas; ++i) {
+        std::cin >> marcas[i]; 
+    }
+
+    tSol sol = resolver(marcas);
+
+    // escribir sol
+    std::cout << sol.maxPruebasMalas << " " << sol.comienzo << '\n'; 
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("21sample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
